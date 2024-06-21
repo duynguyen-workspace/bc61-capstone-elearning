ed1104329430ee948e4f87c114f441c7a4b588a1
HuyLuong <huyluong131001@gmail.com>
2024-06-21 21:49:31 +0700

    full course and home

--- src/components/CoursesItem/CourseItem.jsx
+++ src/components/CoursesItem/CourseItem.jsx
@@ -27,10 +27,12 @@ const CourseItem = () => {
                     <hr className="mt-0 mb-4" />
                 </div>
             </div>
-            <div className="courseItem__item">
-                <div className="courseItem__title">
-                    <h3>{courseCategory}</h3>
-                    <div className="flex items-center">
+            <div className="courseItem__item w-[1170px] mx-auto">
+                <div className="courseItem__title ">
+                    <h3 className="text-white text-5xl  font-bold mt-20">
+                        {courseCategory}
+                    </h3>
+                    <div className="flex items-center text-[#8e88a8] my-6">
                         <div>
                             <i className="fa-solid fa-book-open"></i>
                             <span>3+ Free Courses</span>
@@ -40,8 +42,42 @@ const CourseItem = () => {
                             <span>200+ Student</span>
                         </div>
                     </div>
+                    <p className="text-[#8e88a8] leading-6">
+                        Embark on a journey of knowledge and skill development
+                        with our online course marketplace. Discover a diverse
+                        array of expert-led courses designed to empower and
+                        enrich your learning experience. From professional
+                        development to personal growth, explore our curated
+                        selection of online courses that cater to your unique
+                        aspirations.
+                    </p>
                 </div>
-                <div className="course__show"></div>
+                <div className="course__show grid grid-cols-4 gap-8 mt-20">
+                    {arrCourses.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <div className="mt-7">
+                                    <img
+                                        className="w-full h-[200px]"
+                                        src={item.hinhAnh}
+                                        alt="course"
+                                    />
+                                </div>
+                                <div className="flex justify-between text-[#8e88a8] mt-5">
+                                    <div>
+                                        {item.soLuongHocVien}
+                                        <span>Students</span>
+                                    </div>
+                                    <div>{item.ngayTao}</div>
+                                </div>
+                                <p className="text-[#e8e2ff] font-bold text-xl my-3">
+                                    {item.tenKhoaHoc}
+                                </p>
+                                <p className="text-[#8e88a8] line-clamp-4">{item.moTa}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
